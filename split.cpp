62915a01061ea63e3d1bc9b78d91bf4ec1b66ec1
nnard1616 <nnard1616@gmail.com>
2017-05-03 23:10:57 -0500

    refine questions on split.cpp

--- split.cpp
+++ split.cpp
@@ -1,21 +1,22 @@
 #include "split.hpp"
+//Create a function that behaves like python's split()
 
-template <typename Out>
+template <typename Out> 
 void split(
     const std::string& s,
     char delim,
     Out result)  // why do I need to use a template here and not a vector?
 {
     std::stringstream ss;
-    ss.str(s);  // what's going on here?
+    ss.str(s);  
     std::string item;
     while (std::getline(ss, item, delim))
-        *(result++) = item;  // what's going on here?
+        *(result++) = item;  // what's going on here in the context of back_inserter?
 }
 
 std::vector<std::string> split(const std::__cxx11::string& s, char delim)
 {
     std::vector<std::string> elems;
-    split(s, delim, std::back_inserter(elems));  // back_inserter??
+    split(s, delim, std::back_inserter(elems));  // back_inserter?  Is this feeding split an iterator?
     return elems;
 }
